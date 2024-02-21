dddbdaca6e37865e82a5d91901ed52b5b50749b1
JGJT1 <jjales725@gmail.com>
2024-02-21 19:53:55 -0300

    basico

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('menu-btn').addEventListener('click', function(){
+    document.getElementById('main-menu').style.width = '250px';
+})
+document.getElementById('fechar-menu').addEventListener('click', function(){
+    document.getElementById('main-menu').style.width = '0'
+})
